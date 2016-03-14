237f72dc29b3362ea498adf41b663528e2156cf9
Ali Motevallian <amotevallian@odecee.com.au>
2016-03-14 23:33:51 +1100

    Cleaning systemjs

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,6 @@
  * Created by amotevallian on 27/02/2016.
  */
 
-//import System from 'systemjs';
 import fs from 'fs';
 import _ from 'lodash';
 
@@ -10,18 +9,8 @@ const ROUTE_FILE_MATCHER = /controller\.js$/;
 const API_BASE_PATH = '/api';
 
 function configRoutes(app) {
-// routes
-
-    //return System.import('/Users/amotevallian/dev/node-api-template-app/app/user-management/user-controller.js')
-    //    .then((loadedModule) => {
-    //        console.log(`file loaded!! ${JSON.stringify(loadedModule)}`);
-    //    }).catch((err) => {
-    //        console.log(`err: ${err}`);
-    //    });
-
     let routesFileList = getRoutesList(__dirname);
     return loadRoutes(routesFileList, app);
-
 }
 
 function loadRoutes(fileList, app) {
@@ -46,8 +35,6 @@ function getRoutesList(dirName) {
             if (stat && stat.isDirectory()) {
                 return getRoutesList(file);
             } else if (file.match(ROUTE_FILE_MATCHER) !== null) {
-                //let filenameNoSuffix = file.substring(0, file.lastIndexOf(ROUTE_FILE_SUFFIX));
-                console.log(`filename: ${file}`);
                 return [file];
             } else {
                 return null;
@@ -58,11 +45,3 @@ function getRoutesList(dirName) {
 }
 
 export default configRoutes;
-
-/*
- System.import(file).then((loadedRoute) => {
- app.use(API_BASE_PATH, loadedRoute);
- }).catch((err) => {
- console.log(`Error while loading file: ${file}.\n\n err: ${err}`);
- })
- */
